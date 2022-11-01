a42cf54428f5fdec134f2eb81cd23b1dc625a0e1
Nitrida <71544383+Nitrida@users.noreply.github.com>
2022-11-01 15:40:55 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,44 +3,25 @@ from main import CashCalculator, CaloriesCalculator, Record, Calculator
 
 def test_record_creation():
     com="sdfds"
-    Record(100, 'comment', '31/03/2021')
-    Record(205, 'comment', '21/04/2022')
-    if len(Record.comment)==0:
-        assert com == '[Нехватает коментариев]'
+    test_record = Record(100, 'comment', '31/03/2021')
+    test_record = Record(205, 'comment', '21/04/2022')
+    if len(test_record.comment) == 0:
+        print('[Нехватает коментариев]')
 
 
 def test_cash_calculator():
     cash_calc = CashCalculator(1000)
-    # cash_calc.add_record(Record(amount=200, comment='На вкусняшки'))
-    # cash_calc.add_record(Record(amount=350, comment='Ередит на китайский айфон'))
+    cash_calc.add_record(Record(amount=200, comment='На вкусняшки'))
+    cash_calc.add_record(Record(amount=350, comment='Ередит на китайский айфон'))
+    cash_calc.add_record(Record(amount=450, comment='Ередит на китайский айфон'))
     # return cash_calc.get_today_cash_remained()
-    if Record.amount>0 and len(Record.comment)>0 :
-        assert cash_calc == '[Еще осталось на покушать]'
-    if len(Record.comment)==0:
-        assert cash_calc == '[Забыли коментарий]'
-    if CashCalculator.spent_today <= 0:
-        assert cash_calc == '[Денег нет]'
+    assert cash_calc == 'Денег нет, держись'
 
 
 def test_cal_calculator():
-    calories_cal = CaloriesCalculator(2000)
-    # calories_cal.add_record(Record(amount=1186, comment='Вкусняшка номер раз'))
-    # calories_cal.add_record(Record(amount=84, comment='Вкусняшка номер двас', ))
-    if Record.amount>0 and len(Record.comment)>0 :
-        assert calories_cal == '[Можно сьесть еще чуть чуть]'
-    if len(Record.comment)==0:
-        assert calories_cal == '[Забыли коментарий]'
-    if CaloriesCalculator.spent_today <= 0:
-        assert calories_cal == '[Хватит есть]'
-
-# def test_cashCalc(cash_calc):
-#     assert cash_calculator == 'Ваш остаток 730.0 Rub'
-
-# def test_CalCalc(calories_cal):
-#     assert calories_cal == '[Хватит есть]'
-
-
-
-
-
+    calories_cal = CaloriesCalculator(1000)
+    calories_cal.add_record(Record(amount=450, comment='Вкусняшка номер раз'))
+    calories_cal.add_record(Record(amount=250, comment='Вкусняшка номер двас' ))
+    calories_cal.add_record(Record(amount=250, comment='Вкусняшка номер трис'))
+    assert calories_cal == 'Хватит есть'
 

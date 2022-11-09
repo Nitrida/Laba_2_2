924938a2bb6e2c407e070a6dda9f2206a7b80c1c
Nitrida <71544383+Nitrida@users.noreply.github.com>
2022-11-09 19:15:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,34 @@
 import pytest
-import main
-from main import CashCalculator, CaloriesCalculator, Record
-
-def test_record_creation():
-    # test_record = Record(100, 'comment', '31/03/2021')
-    # test_record = Record(205, 'comment', '21/04/2022')
-    if len(main.com) == 0:
-        print('[Нехватает коментариев]')
+from cl import CashCalculator, CaloriesCalculator, Record
 
 
-def test_cash_calculator():
+@pytest.fixture()
+def cash_calculator():
     cash_calc = CashCalculator(1000)
     cash_calc.add_record(Record(amount=200, comment='На вкусняшки'))
     cash_calc.add_record(Record(amount=350, comment='Ередит на китайский айфон'))
     cash_calc.add_record(Record(amount=450, comment='Ередит на китайский айфон'))
-    # return cash_calc.get_today_cash_remained()
-    assert cash_calc == 'Денег нет, держись'
+    return cash_calc.get_today_cash_remained
 
 
-def test_cal_calculator():
+@pytest.fixture()
+def cal_calculator():
     calories_cal = CaloriesCalculator(1000)
     calories_cal.add_record(Record(amount=450, comment='Вкусняшка номер раз'))
     calories_cal.add_record(Record(amount=250, comment='Вкусняшка номер двас' ))
     calories_cal.add_record(Record(amount=250, comment='Вкусняшка номер трис'))
-    assert calories_cal == 'Хватит есть'
+    return calories_cal
 
+
+def test_cashCalc(cash_calculator):
+    assert cash_calculator('usd') == 'Денег нет, держись'
+
+def test_cashCalc2(cash_calculator):
+    assert cash_calculator('rub') == 'Денег нет, держись'
+
+def test_CalCalc(cal_calculator):
+    assert cal_calculator.get_calories_remained() == 'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 50 кКал'
+
+def test_calCalc2(cal_calculator):
+    cal_calculator.add_record(Record(amount=1450, comment='Торт'))
+    assert cal_calculator.get_calories_remained() == 'Хватит есть!'
